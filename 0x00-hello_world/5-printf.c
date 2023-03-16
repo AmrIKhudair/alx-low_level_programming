795d198c315198e26f5c2aba970084d7b3a1cff5
Amr Ibrahim Khudair <amrikhudair@outlook.com>
2023-03-16 04:32:20 -0700

    Add 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Defines the main funtion
+ *
+ * Return: 0 if runs successfully, otherwise int if error occures
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
